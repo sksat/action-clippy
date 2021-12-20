4b24fa3e024d945c3352a998c996913c74bc8891
sksat <sksat@sksat.net>
2021-12-21 05:23:46 +0900

    add: first impl

---
+++ entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -e
+
+cargo clippy --message-format json ${INPUT_CLIPPY_FLAGS} 2>&1 \
+  | clippy-reviewdog-filter \
+  | reviewdog \
+    -f=checkstyle \
+    -name="${INPUT_TOOL_NAME}" \
+    -level="${INPUT_LEVEL}" \
+    -reporter="${INPUT_REPORTER:-github-pr-check}" \
+    -filter-mode="${INPUT_FILTER_MODE}" \
+    -fail-on-error="${INPUT_FAIL_ON_ERROR}" \
+    ${INPUT_REVIEWDOG_FLAGS}
