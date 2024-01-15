471d119a27079d235785c6f609f5deb7a962e1e1
sksat <sksat@sksat.net>
2024-01-15 19:00:25 +0900

    split clippy-reviewdog-filter pipe to mitigate broken pipe

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,11 +7,14 @@ cargo clippy --message-format human ${INPUT_CLIPPY_FLAGS}
 
 cargo clippy --message-format json ${INPUT_CLIPPY_FLAGS} 2>&1 \
   | clippy-reviewdog-filter \
-  | reviewdog \
+  > /tmp/clippy-checkstyle.xml
+
+reviewdog \
     -f=checkstyle \
     -name="${INPUT_TOOL_NAME}" \
     -level="${INPUT_LEVEL}" \
     -reporter="${INPUT_REPORTER:-github-pr-check}" \
     -filter-mode="${INPUT_FILTER_MODE}" \
     -fail-on-error="${INPUT_FAIL_ON_ERROR}" \
-    ${INPUT_REVIEWDOG_FLAGS}
+    ${INPUT_REVIEWDOG_FLAGS} \
+    < /tmp/clippy-checkstyle.xml
